1989216cae46ee81a5140b593f598a9e66577d10
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2025-06-09 12:54:18 +0000

    played with functions and parameters

--- Week_4/lesson/script.js
+++ Week_4/lesson/script.js
@@ -6,3 +6,10 @@ function changeText() {
         heading.innerHTML = "JavaScript";
     }
 }
+
+function addNumbers(num1, num2) {
+    document.getElementById("addition").innerHTML = num1 + num2;
+}
+
+// usage
+addNumbers(10, 7);
