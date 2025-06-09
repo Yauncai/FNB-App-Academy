2cfabdbd959a7ea585ade9d1b9b234aac0165a7c
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2025-06-09 13:40:19 +0000

    played with switch statement by creating a weather demo

--- Week_4/lesson/script.js
+++ Week_4/lesson/script.js
@@ -1,14 +1,24 @@
-// Global 
-var globalMessage = "I am a global variable!";
-
-function showScope() {
-    // Local 
-    var localMessage = "I am a local variable!";
-    document.getElementById("scope-demo").innerHTML =
-        globalMessage + "<br>" + localMessage;
+function showWeatherQuote() {
+    var weather = document.getElementById("weather").value;
+    var quote = "";
+    switch (weather) {
+        case "sunny":
+            quote = "It's a bright and beautiful day!";
+            break;
+        case "rainy":
+            quote = "Don't forget your umbrella!";
+            break;
+        case "cloudy":
+            quote = "A perfect day to relax indoors.";
+            break;
+        case "windy":
+            quote = "Hold onto your hat!";
+            break;
+        case "snowy":
+            quote = "Time for some snow fun!";
+            break;
+        default:
+            quote = "";
+    }
+    document.getElementById("weather-quote").innerHTML = quote;
 }
-
-showScope();
-
-
-document.getElementById("scope-demo").innerHTML = localMessage; // Uncaught ReferenceError
