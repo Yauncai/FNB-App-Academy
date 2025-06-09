ab136a7f2ec21b6d3309a2c8c781e14e63ab9e4b
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2025-06-09 11:59:14 +0000

    playing with js variables

--- Week_4/lesson/script.js
+++ Week_4/lesson/script.js
@@ -1,2 +1,8 @@
-document.getElementById(“heading1”).innerHTML = (“This is JavaScript!”);
-document.getElementById(“paragraph1”).innerHTML = (“I’m having fun!”);
+var text1;
+var text2;
+var text3;
+text1 = “Hello everyone on the planet”;
+text2 = “I love learning JS!”;
+text3 = “ JS is awesome!”;
+document.getElementById(“heading1”).innerHTML = (text1);
+document.getElementById(“paragraph1”).innerHTML = (text2 + text3);
