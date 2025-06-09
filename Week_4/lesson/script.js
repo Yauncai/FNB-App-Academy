8830fcfdc0ce299d1eb9c87cf627086a5f968444
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2025-06-09 12:46:41 +0000

    played with functions

--- Week_4/lesson/script.js
+++ Week_4/lesson/script.js
@@ -1,7 +1,3 @@
-var userInputPassword = "008";
-var actualPassword = "007";
-if(userInputPassword == actualPassword){
-document.getElementById("output").innerHTML = ("Correct password");
-}else{
-document.getElementById("output").innerHTML = ("Incorrect password. Try again");
+function changeText() {
+    document.getElementById("myHeading").innerHTML = "is awesome";
 }
